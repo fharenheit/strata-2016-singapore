bd96520fd71760ec9b6830e7a17c9ea23d239781
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2016-09-21 21:35:03 -0700

    Update LinearRegressionWithEncoding.scala

--- src/main/scala/org/apache/spark/LinearRegressionWithEncoding.scala
+++ src/main/scala/org/apache/spark/LinearRegressionWithEncoding.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+package org.apache.spark
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
