4da64c2b747ffcc2ac91ba423f585b98efe47aa3
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2016-09-24 11:10:16 -0700

    Update KMeansExample.scala

--- src/main/scala/org/strata/workshop/KMeansExample.scala
+++ src/main/scala/org/strata/workshop/KMeansExample.scala
@@ -23,8 +23,7 @@ package org.strata.workshop
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.sql._
-
-//import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types._
 
 import org.apache.spark.ml.feature.StandardScaler
 
