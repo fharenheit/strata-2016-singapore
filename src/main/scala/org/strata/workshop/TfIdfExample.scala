4094dacdb995586a9747474132aa47ac19827bdf
김병곤 <fharenheit@ex-em.com>
2016-12-06 12:25:09 +0800

    포맷팅

---
+++ src/main/scala/org/strata/workshop/TfIdfExample.scala
@@ -0,0 +1,44 @@
+package org.strata.workshop
+
+// $example on$
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+
+// $example off$
+import org.apache.spark.sql.SparkSession
+
+object TfIdfExample {
+
+    def main(args: Array[String]) {
+        val spark = SparkSession
+            .builder
+            .master("local")
+            .appName("TfIdfExample")
+            .getOrCreate()
+
+        // $example on$
+        val sentenceData = spark.createDataFrame(Seq(
+            (0.0, "Hi I heard about Spark"),
+            (0.0, "I wish Java could use case classes"),
+            (1.0, "Logistic regression models are neat")
+        )).toDF("label", "sentence")
+
+        val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
+        val wordsData = tokenizer.transform(sentenceData)
+        wordsData.show(false)
+
+        val hashingTF = new HashingTF()
+            .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(20)
+
+        val featurizedData = hashingTF.transform(wordsData)
+        // alternatively, CountVectorizer can also be used to get term frequency vectors
+
+        val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+        val idfModel = idf.fit(featurizedData)
+
+        val rescaledData = idfModel.transform(featurizedData)
+        rescaledData.select("label", "features").show()
+        // $example off$
+
+        spark.stop()
+    }
+}
